0262376071b0053b70e6668a0daa376d81cb27ef
putriaditya <putriaditya166@gmail.com>
2022-09-15 09:20:39 +0700

    status succes

--- resources/views/admin/users/home.blade.php
+++ resources/views/admin/users/home.blade.php
@@ -1,23 +1,7 @@
 @extends('layout.app')
 
 @section('body')
-    @if (session('status'))
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            <strong>{{ session('status') }}</strong>
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-    @endif
-
-    @foreach ($errors->all() as $error)
-        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-            <strong>{{ $error }}</strong>
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-    @endforeach
+    
     <!-- Basic Tables start -->
     <section class="section">
         <div class="card">
--- resources/views/admin/users/update.blade.php
+++ resources/views/admin/users/update.blade.php
@@ -2,23 +2,7 @@
 @extends('layout.app')
 
 @section('body')
-@if (session('status'))
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            <strong>{{ session('status') }}</strong>
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-    @endif
 
-    @foreach ($errors->all() as $error)
-        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-            <strong>{{ $error }}</strong>
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-    @endforeach
     <section class="section">
         <div class="card">
             <div class="card-header">
