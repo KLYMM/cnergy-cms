c4cfa9f5d4ccabb4cc8d767fc3c098f7a2418ca5
yandiaan <diansetiawan2121@gmail.com>
2023-03-21 12:50:22 +0700

    fix status sidebar when url have query params

--- resources/views/components/sidebar-sub-menu.blade.php
+++ resources/views/components/sidebar-sub-menu.blade.php
@@ -1,13 +1,17 @@
+@php
+    $path = explode("?", request()->getRequestUri()); 
+@endphp
+
 <li
     class="submenu-item 
     @if (count($item['children'])) sidebar-item has-sub subs-child @endif
     " data-parent-sidebar="{{ $item['parent_id'] }}" data-id-sidebar="{{ $item['id'] }}" 
-    @if (str_contains(request()->getRequestUri() . "/", $item['slug']) && count($item['children']) == 0) data-status-sidebar="true" @endif>
+    @if (str_contains($path[0] . "/", $item['slug']) && count($item['children']) == 0) data-status-sidebar="true" @endif>
     <a @if (count($item['children'])) class="sidebar-link"
         href="{{ url($item['slug']) }}" 
     @else
         href="{{ url($item['slug']) }}" 
-        @if (str_contains(request()->getRequestUri() . "/", $item['slug'])) style="background-color: #f0f1f5 !important; border-radius: 7px" @endif
+        @if (str_contains($path[0] . "/", $item['slug'])) style="background-color: #f0f1f5 !important; border-radius: 7px" @endif
     @endif
         >
         {{ $item['menu_name'] }}
